e815bfef984458f60c1ff1b050ff65cbcb8c85a3
jirolique8-oss <jirolique8@gmail.com>
2026-05-20 23:22:41 +0800

    Create Book class

---
+++ book.py
@@ -0,0 +1,36 @@
+"""Book class for the Library Management System."""
+
+
+class Book:
+    """Represents a book in the library."""
+    
+    def __init__(self, book_id, title, author):
+        """
+        Initialize a Book object.
+        
+        Args:
+            book_id (str): Unique identifier for the book
+            title (str): Title of the book
+            author (str): Author of the book
+        """
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.available = True
+    
+    def borrow(self):
+        """Mark the book as borrowed."""
+        self.available = False
+    
+    def return_book(self):
+        """Mark the book as returned."""
+        self.available = True
+    
+    def __str__(self):
+        """String representation of the book."""
+        status = "Available" if self.available else "Borrowed"
+        return f"{self.book_id} - {self.title} by {self.author} [{status}]"
+    
+    def __repr__(self):
+        """Developer-friendly representation."""
+        return f"Book({self.book_id}, {self.title}, {self.author}, available={self.available})"
